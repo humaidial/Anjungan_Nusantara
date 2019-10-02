9499e5bd3e12df714d0d86177b6c1bda39ef9ace
Afirsta <afirsta15@gmail.com>
2019-10-02 20:54:16 +0700

    view admin

---
+++ application/controllers/Halamanadmin.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Halamanadmin extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Profile_model');
+		$this->load->model('Login_model');
+	}
+
+	public function berandaadmin()
+	{
+		$this->load->view('admin/Halamanadmin');
+	}
+
+	
+
+}
