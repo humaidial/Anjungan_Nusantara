a0fb6c6a9a283c17d53ad75f0413ded4dd0f22f1
humaidi <humaidialmasmed@gmail.com>
2019-10-16 22:34:27 +0700

    Update head.php

--- application/views/penjual/_partials/head.php
+++ application/views/penjual/_partials/head.php
@@ -2,7 +2,10 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-<title><?php echo SITE_NAME .": ". ucfirst($this->uri->segment(1)) ." - ". ucfirst($this->uri->segment(2)) ?></title>
+<title>Halaman Penjual</title>
+
+<link rel="apple-touch-icon" href="<?php echo base_url('assets/templateadmin/base/assets/images/apple-touch-icon.png')?>">
+<link rel="shortcut icon" href="<?php echo base_url('assets/templateadmin/base/assets/images/favicon.ico')?>">
 
 <!-- Bootstrap core CSS-->
 <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
